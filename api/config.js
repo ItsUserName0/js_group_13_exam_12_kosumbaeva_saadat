476046c949acd49289fef839e1e9dd4739e0725f
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-04-09 11:50:09 +0600

    App initial commit.

--- api/config.js
+++ api/config.js
@@ -6,11 +6,11 @@ module.exports = {
   rootPath,
   uploadPath: path.join(rootPath, 'public'),
   mongo: {
-    db: 'mongodb://localhost/project-template',
+    db: 'mongodb://localhost/gallery-app',
     options: {useNewUrlParser: true},
   },
   facebook: {
-    appId: '',
-    appSecret: '',
+    appId: '522171076196691',
+    appSecret: '80dd03bb34a4a3150e193af4b8bf6627',
   },
 };
